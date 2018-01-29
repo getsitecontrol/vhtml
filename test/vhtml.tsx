ae82bfc2e7c42ced014b68e5fbbc870531ba41d6
arusanov <alexander.rusanov@gmail.com>
2018-01-29 12:56:45 +0300

    rewrite in ts

--- test/vhtml.tsx
+++ test/vhtml.tsx
@@ -1,133 +1,117 @@
-import {h,options} from '../src/vhtml';
-import { expect } from 'chai';
+const { expect } =require('chai')
+const { h, options } = require('../src/vhtml')
 /** @jsx h */
 /*global describe,it*/
 
 describe('vhtml', () => {
 	it('should stringify html', () => {
-		let items = ['one', 'two', 'three'];
+		let items = ['one', 'two', 'three']
 		expect(
 			<div class="foo">
 				<h1>Hi!</h1>
 				<p>Here is a list of {items.length} items:</p>
-				<ul>
-					{ items.map( item => (
-						<li>{ item }</li>
-					)) }
-				</ul>
+				<ul>{items.map(item => <li>{item}</li>)}</ul>
 			</div>
 		).to.equal(
 			`<div class="foo"><h1>Hi!</h1><p>Here is a list of 3 items:</p><ul><li>one</li><li>two</li><li>three</li></ul></div>`
-		);
-	});
+		)
+	})
 
 	it('should sanitize children', () => {
 		expect(
 			<div>
-				{ `<strong>blocked</strong>` }
+				{`<strong>blocked</strong>`}
 				<em>allowed</em>
 			</div>
 		).to.equal(
 			`<div>&lt;strong&gt;blocked&lt;/strong&gt;<em>allowed</em></div>`
-		);
-	});
+		)
+	})
 
 	it('should sanitize attributes', () => {
-		expect(
-			<div onclick={`&<>"'`} />
-		).to.equal(
+		expect(<div onclick={`&<>"'`} />).to.equal(
 			`<div onclick="&amp;&lt;&gt;&quot;&apos;"></div>`
-		);
-	});
+		)
+	})
 
 	it('should flatten children', () => {
 		expect(
 			<div>
-				{[['a','b']]}
-				<c>d</c>
-				{['e',['f'],[['g']]]}
+				{[['a', 'b']]}
+				<span>d</span>
+				{['e', ['f'], [['g']]]}
 			</div>
-		).to.equal(
-			`<div>ab<c>d</c>efg</div>`
-		);
-	});
+		).to.equal(`<div>ab<span>d</span>efg</div>`)
+	})
 
 	it('should support sortof components', () => {
-		let items = ['one', 'two'];
+		let items = ['one', 'two']
 
-		const Item = ({ item, index, children }) => (
+		interface Props {
+			item: string,
+			index: number,
+			children?:JSX.Element[]
+		}
+
+		const Item = ({ item, index, children }:Props) => (
 			<li id={index}>
 				<h4>{item}</h4>
 				{children}
 			</li>
-		);
+		)
 
 		expect(
 			<div class="foo">
 				<h1>Hi!</h1>
 				<ul>
-					{ items.map( (item, index) => (
-						<Item {...{ item, index }}>
-							This is item {item}!
-						</Item>
-					)) }
+					{items.map((item, index) => (
+						<Item {...{ item, index }}>This is item {item}!</Item>
+					))}
 				</ul>
 			</div>
 		).to.equal(
 			`<div class="foo"><h1>Hi!</h1><ul><li id="0"><h4>one</h4>This is item one!</li><li id="1"><h4>two</h4>This is item two!</li></ul></div>`
-		);
-	});
+		)
+	})
 
 	it('should support sortof components without args', () => {
-	  let items = ['one', 'two'];
+		let items = ['one', 'two']
 
-	  const Item = () => (
-	    <li>
-	      <h4></h4>
-	    </li>
-	  );
+		const Item = () => (
+			<li>
+				<h4 />
+			</li>
+		)
 
-	  expect(
-	    <div class="foo">
-	      <h1>Hi!</h1>
-	      <ul>
-	        { items.map( (item, index) => (
-	          <Item>
-	            This is item {item}!
-	          </Item>
-	        )) }
-	      </ul>
-	    </div>
-	  ).to.equal(
-	    `<div class="foo"><h1>Hi!</h1><ul><li><h4></h4></li><li><h4></h4></li></ul></div>`
-	  );
-	});
+		expect(
+			<div class="foo">
+				<h1>Hi!</h1>
+				<ul>{items.map((item, index) => <Item>This is item {item}!</Item>)}</ul>
+			</div>
+		).to.equal(
+			`<div class="foo"><h1>Hi!</h1><ul><li><h4></h4></li><li><h4></h4></li></ul></div>`
+		)
+	})
 
 	it('should support sortof components without args but with children', () => {
-	  let items = ['one', 'two'];
+		let items = ['one', 'two']
 
-	  const Item = ({ children }) => (
-	    <li>
-	      <h4></h4>
-	      {children}
-	    </li>
-	  );
+		const Item = ({ children }:{children?:JSX.Element[]}) => (
+			<li>
+				<h4 />
+				{children}
+			</li>
+		)
 
-	  expect(
-	    <div class="foo">
-	      <h1>Hi!</h1>
-	      <ul>
-	        { items.map( (item, index) => (
-	          <Item>
-	            This is item {item}!
-	          </Item>
-	        )) }
-	      </ul>
-	    </div>
-	  ).to.equal(
-	    `<div class="foo"><h1>Hi!</h1><ul><li><h4></h4>This is item one!</li><li><h4></h4>This is item two!</li></ul></div>`
-	  );
-	});
+		expect(
+			<div class="foo">
+				<h1>Hi!</h1>
+				<ul>{items.map((item, index) => <Item>This is item {item}!</Item>)}</ul>
+			</div>
+		).to.equal(
+			`<div class="foo"><h1>Hi!</h1><ul><li><h4></h4>This is item one!</li><li><h4></h4>This is item two!</li></ul></div>`
+		)
+	})
 
 	it('should support empty (void) tags', () => {
 		expect(
@@ -155,32 +139,26 @@ describe('vhtml', () => {
 			</div>
 		).to.equal(
 			`<div><area><base><br><col><command><embed><hr><img><input><keygen><link><meta><param><source><track><wbr><div></div><span></span><p></p></div>`
-		);
-	});
+		)
+	})
 
 	it('should handle special prop names', () => {
-		expect(
-			<div className="my-class" htmlFor="id" />
-		).to.equal(
+		expect(<div className="my-class" htmlFor="id" />).to.equal(
 			'<div class="my-class" for="id"></div>'
-		);
-	});
+		)
+	})
 
 	it('should handle camelCased prop names', () => {
-		expect(
-			<div onMouseLeave="open()" tabIndex={1} htmlFor="id" />
-		).to.equal(
+		expect(<div onmouseleave="open()" tabIndex={1} htmlFor="id" />).to.equal(
 			'<div onmouseleave="open()" tabindex="1" for="id"></div>'
-		);
-	});
+		)
+	})
 
 	it('should handle options', () => {
-		options.normalizeNode = (nodeName)=>nodeName.toUpperCase();
-		options.normalizeAttr = (nodeName)=>nodeName.toUpperCase();
-		expect(
-			<div onMouseLeave="open()" tabIndex={1} htmlFor="id" />
-		).to.equal(
+		options.normalizeNode = nodeName => nodeName.toUpperCase()
+		options.normalizeAttr = nodeName => nodeName.toUpperCase()
+		expect(<div onmouseleave="open()" tabIndex={1} htmlFor="id" />).to.equal(
 			'<DIV ONMOUSELEAVE="open()" TABINDEX="1" FOR="id"></DIV>'
-		);
-	});
-});
+		)
+	})
+})
